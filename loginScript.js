e2097dcafa739ebe4e27a7a753611543a4405395
SeenivasanSeeni <seeni0424@gmail.com>
2018-07-20 11:52:09 +0530

    capthca added

--- loginScript.js
+++ loginScript.js
@@ -1,6 +1,20 @@
 var loginButton=document.getElementById("login");
 var username=document.getElementById("username");
 var password=document.getElementById("password");
+var captchaText=document.getElementById("captchaText");
+var captchaInput=document.getElementById("captcha");
+
+//Enter Random Text in Captcha
+var  getRandomCaptcha=function(){
+        var captcha="";
+        var options=['A','B']
+        for(let i=0;i<10;i++){
+            captcha+=Math.floor(Math.random()*10);
+        }
+        captchaText.textContent=captcha;
+};
+getRandomCaptcha();
+
 loginButton.addEventListener("click",function(e){
     e.preventDefault();
     const today=new Date();
