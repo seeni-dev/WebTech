87210530f786459c5c66225672460e2b4a4b71bd
seeni <seeni0424@gmail.com>
2018-07-27 06:11:06 -0700

    Added username and password

--- registerUser.pl
+++ registerUser.pl
@@ -1,1 +1,16 @@
-print "Hello world"
+#!C:\xampp\perl\bin\perl.exe
+print "Content-type: text/html\n\n";
+use CGI;
+use CGI::Carp qw(fatalsToBrowser);
+$cgi=new CGI;
+read(STDIN,$FormData,$ENV{'CONTENT-LENGTH'});
+
+$name=$cgi->param("name");
+$password=$cgi->param("password");
+
+open(file,">","FILES/users.txt");
+print "NAME:$name<br>";
+print file "NAME:$name\n";
+
+print "PASSWORD:$password<br>";
+print file "PASSWORD:$password\n";
