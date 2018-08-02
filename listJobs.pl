84dd305137bf7326518718dee38b6a428f6e46e3
seeni <seeni0424@gmail.com>
2018-08-02 16:25:20 -0700

    this version creates a bomb

--- listJobs.pl
+++ listJobs.pl
@@ -9,17 +9,33 @@ $qualification=$cgi->param('qualification');
 
 
 open(ifile,"<","FILES/jobs.txt");
+open(ofile,">","FILES/jobs.txt.temp");
 
 $line=<ifile>; # disgard this feed
+print ofile $line;
 $line=<ifile>;
+print "<br /> Available Jobs for your Qualification are <br /><br />";
 while($line ne ""){
     @arr=split(/ /,$line);
     $line=<ifile>;
     if(@arr[0] eq $qualification && @arr[2]!=0){
-        print @arr;
+        print "Qualification: @arr[0]<br/>Location: @arr[1]<br/> Available jobs: @arr[2] <br/>";
+        print "<br/><br />";
+        @arr[2]=@arr[2]-1;
     }
-    print "<br />";
+    print ofile "@arr[0] @arr[1] @arr[2]\n";
+
 }
 
-
 close(ifile);
+close(ofile);
+# recopy the content of files
+open(ifile,"<","FILES/jobs.txt.temp");
+open(ofile,">","FILES/jobs.txt");
+$line=<ifile>;
+while ($line ne ""){
+    print ofile $line;
+}
+close(ifile);
+close(ofile);
+
