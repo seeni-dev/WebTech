6595d05ba46f4268a101f1c26988df12b4fa6290
seeni <seeni0424@gmail.com>
2018-08-10 06:19:06 -0700

    Done problem with ]n

--- listJobs.pl
+++ listJobs.pl
@@ -12,19 +12,22 @@ open(ifile,"<","FILES/jobs.txt");
 open(ofile,">","FILES/jobs.txt.temp");
 
 $line=<ifile>; # disgard this feed
+chomp($line);
 print ofile $line;
 $line=<ifile>;
+chomp($line);
 print "<br /> Available Jobs for your Qualification are <br /><br />";
 while($line ne ""){
+    print ofile "\n";
     @arr=split(/ /,$line);
     $line=<ifile>;
+    chomp($line);
     if(@arr[0] eq $qualification && @arr[2]!=0){
         print "Qualification: @arr[0]<br/>Location: @arr[1]<br/> Available jobs: @arr[2] <br/>";
         print "<br/><br />";
         @arr[2]=@arr[2]-1;
     }
-    print ofile "@arr[0] @arr[1] @arr[2]\n";
-
+    print ofile "@arr[0] @arr[1] @arr[2]";
 }
 
 close(ifile);
