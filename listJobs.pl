9a10814d4bad13b02480aab807306bfd54ffa78a
seeni <seeni0424@gmail.com>
2018-08-27 11:57:00 -0700

    Working select *

--- listJobs.pl
+++ listJobs.pl
@@ -7,17 +7,27 @@ read(STDIN,$FormData,$ENV{'CONTENT-LENGTH'});
 
 $qualification=$cgi->param('qualification');
 
-#Windows-based Perl/DBI/MS Access example
 use DBI;
-
-my @drivers = DBI->available_drivers;
-print join(", ", @drivers), "\n";
-
-
-
-
 
 my $dbh = DBI->connect('dbi:ODBC:driver=Microsoft Access Driver (*.mdb, *.accdb);dbq=C:\Users\Seeni\Documents\JobKar.accdb', 'root','') or 
   die "$DBI::errstr\n";
 
+print("Connection Sucessful<br />");
+
+$sth=$dbh->prepare("Select * from Jobs;");
+
+$sth->execute();
+
+while(my @row  = $sth->fetchrow_array()){
+  print "@row[0] @row[1] @row[2] <br />";
+  if(@row[0] eq $qualification){
+    print("qual is equal <br />");
+  }
+}
+
 print "Connected";
+
+
+print( "DEBUG<br />");
+$DSN = 'driver=Microsoft Access Driver(*.mdb,*.accdb);dbq=C:\Users\Seeni\Documents\JobKar.accdb';
+print("dbi:ODBC:$DSN");
